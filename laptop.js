691915801aa7a66beaa4b314170eaaa0e5ead6b3
Karthikkrishnan-J <118099100+Karthikkrishnan-J@users.noreply.github.com>
2023-09-29 15:28:07 +0400

    Add files via upload

---
+++ laptop.js
@@ -0,0 +1,17 @@
+/*function preload(){
+    img = loadImage("laptop.jpg");
+}
+function setup(){
+    canvas = createCanvas(640, 820);
+    canvas.center();
+}
+function draw(){
+    image(img, 0, 0, 640, 820);
+    stroke("red");
+    noFill();
+    rect(0 ,200 ,640 ,460);
+    text("laptop", 0, 210);
+}
+function back(){
+    window.location = "index.html";
+}*/
